9dd13ea7ad0a7e3933199b56af85396b6bb9d9fa
RemyMTech <contact@remymtech.fr>
2025-05-26 22:23:05 +0200

    Corrige la casse du fichier Client.js

--- routes/inscription.js
+++ routes/inscription.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Client = require('models/client');
+const Client = require('../models/Client');
 
 // Route POST /api/inscription
 router.post('/', async (req, res) => {
