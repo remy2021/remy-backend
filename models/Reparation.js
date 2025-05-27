ec6ef9704dbfb677414711edc14f5cf263065472
RemyMTech <contact@remymtech.fr>
2025-05-27 22:36:35 +0200

    Modèle Reparation complet et sécurisé

--- models/Reparation.js
+++ models/Reparation.js
@@ -1,17 +1,48 @@
 const mongoose = require('mongoose');
 
 const ReparationSchema = new mongoose.Schema({
-  clientId: String,
-  nom: String,
-  prenom: String,
-  email: String,
-  telephone: String,
-  adresse: String,
-  marque: String,
-  modele: String,
-  imei: String,
-  problemes: [String],
-  commentaire: String,
+  clientId: {
+    type: String,
+    required: true
+  },
+  nom: {
+    type: String,
+    required: true
+  },
+  prenom: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  telephone: {
+    type: String,
+    required: true
+  },
+  adresse: {
+    type: String,
+    required: true
+  },
+  marque: {
+    type: String,
+    required: true
+  },
+  modele: {
+    type: String,
+    required: true
+  },
+  imei: {
+    type: String
+  },
+  problemes: {
+    type: [String],
+    required: true
+  },
+  commentaire: {
+    type: String
+  },
   dateDemande: {
     type: Date,
     default: Date.now
